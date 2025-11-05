4c62b3a3e4725a8643ac2adff7b61fd78009f10c
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-05 10:03:55 +0100

    Creacion de las pages hecha

---
+++ src/app/pages/users-create-page/users-create-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-create-page',
+  imports: [],
+  templateUrl: './users-create-page.html',
+  styleUrl: './users-create-page.css',
+})
+export class UsersCreatePage {
+
+}
