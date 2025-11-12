7ab853962362ae04b85f285607ce016183517418
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-12 14:10:53 +0100

    ÃAÃñadido la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,7 @@ import { LoginPage } from './pages/login-page/login-page';
 import { DashboardPage } from './pages/dashboard-page/dashboard-page';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginPage
-    },
-    {
-        path: 'dashboard',
-        component: DashboardPage
-    }
+    {path: 'login', component: LoginPage},
+    { path: 'dashboard', component: DashboardPage},
+    { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
