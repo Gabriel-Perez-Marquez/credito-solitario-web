d81e691b15c6793684d31aac7f1a7b1a6ce610fe
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-13 19:38:00 +0100

    pagina de dashboard hecha finalmente

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Categories{
+  id:number,
+  nombre:String,
+  ganancia: number
+}
+
+
 @Component({
   selector: 'app-dashboard-page',
   imports: [],
@@ -7,5 +14,10 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-page.css',
 })
 export class DashboardPage {
+
+  categories: Categories [] = [
+    { id: 1, nombre: 'Comida', ganancia: 0 },
+    { id: 2, nombre: 'Ropa', ganancia: 0 }
+  ]
 
 }
